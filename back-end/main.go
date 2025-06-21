0e417c4ee18f3bbcbb68c373fa8a4acc673b7c87
Hasiberriak <yutthaphonsonsakul@gmail.com>
2025-06-21 20:56:48 +0700

    Succeed

--- back-end/main.go
+++ back-end/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 	"dashboard/handlers"
 	"dashboard/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 
 	_ "github.com/lib/pq"
 )
@@ -20,15 +21,15 @@ var db *sql.DB
 
 func main() {
 	err := godotenv.Load(".env") // โหลดไฟล์ .env
-    if err != nil {
-        log.Fatalf("Error loading .env file: %v", err)
-    }
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 
 	host := os.Getenv("DB_HOST")
-    port := os.Getenv("DB_PORT") 
-    user := os.Getenv("DB_USER")
-    password := os.Getenv("DB_PASSWORD")
-    dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 	apiPort := os.Getenv("API_PORT")
 
 	portInt, err := strconv.Atoi(port)
@@ -39,27 +40,31 @@ func main() {
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, portInt, user, password, dbname)
-	
-	sdb ,err := sql.Open("postgres", psqlInfo)
+
+	sdb, err := sql.Open("postgres", psqlInfo)
 
 	db = sdb
 
-	
 	if err != nil {
 		log.Fatalf("Error connecting to the database: %v", err)
 	}
-	
+
 	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
 		log.Fatalf("Error pinging the database: %v", err)
 	}
-	
 
 	log.Println("Successfully connected to the database!")
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowHeaders: "Origin,Content-Type,Accept",
+	}))
 
 	transactionHandler := handlers.NewTransactionHandler(db)
 
@@ -69,4 +74,3 @@ func main() {
 	log.Fatal(app.Listen(fmt.Sprintf(":%s", apiPort)))
 
 }
-
